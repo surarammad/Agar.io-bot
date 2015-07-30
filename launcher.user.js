fdb270af81e3f8b0e6fd71e4e826a24df85236aa
surarammad <efsanekral2009@hotmail.com>
2015-07-30 16:59:50 +0300

    Update launcher.user.js

--- launcher.user.js
+++ launcher.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        AposLauncher
 // @namespace   AposLauncher
-// @include     http://agar.io/*
+// @include     http://agario.web.tr/*
 // @version     3.063
 // @grant       none
 // @author      http://www.twitch.tv/apostolique
